613e68234dd1c518d3f541e4ab9e4b14aa8abead
Félix SCHERER <felix.scherer@laposte.net>
2024-11-27 15:06:52 +0100

    chore: cleaned chat route

--- app/(chat)/api/chat/route.ts
+++ app/(chat)/api/chat/route.ts
@@ -1,7 +1,7 @@
 import { convertToCoreMessages, Message, streamText } from "ai";
 import { z } from "zod";
 
-import { geminiProModel } from "@/ai";
+import { geminiFlashModel, geminiProModel } from "@/ai";
 
 // import {
 //   generateReservationPrice,
@@ -19,7 +19,7 @@ export async function POST(request: Request) {
   );
 
   const result = await streamText({
-    model: geminiProModel,
+    model: geminiFlashModel,
     system: `\n
         - Tu es un personnage qui vit un scénario de catastrophe naturelle dans le futur à cause du réchauffement climatique.
         - Ton but est de prévenir des dangers et des risques de la pollution dans la période actuelle.
@@ -28,53 +28,53 @@ export async function POST(request: Request) {
       `,
     messages: coreMessages,
     tools: {
-      getWeather: {
-        description: "Obtenir la météo actuelle à un emplacement",
-        parameters: z.object({
-          latitude: z.number().describe("Coordonnée de latitude"),
-          longitude: z.number().describe("Coordonnée de longitude"),
-        }),
-        execute: async ({ latitude, longitude }) => {
-          const response = await fetch(
-            `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m&hourly=temperature_2m&daily=sunrise,sunset&timezone=auto`,
-          );
+      // getWeather: {
+      //   description: "Obtenir la météo actuelle à un emplacement",
+      //   parameters: z.object({
+      //     latitude: z.number().describe("Coordonnée de latitude"),
+      //     longitude: z.number().describe("Coordonnée de longitude"),
+      //   }),
+      //   execute: async ({ latitude, longitude }) => {
+      //     const response = await fetch(
+      //       `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m&hourly=temperature_2m&daily=sunrise,sunset&timezone=auto`,
+      //     );
 
-          const weatherData = await response.json();
-          return weatherData;
-        },
-      },
-      displayBoardingPass: {
-        description: "Display a boarding pass",
-        parameters: z.object({
-          reservationId: z
-            .string()
-            .describe("Unique identifier for the reservation"),
-          passengerName: z
-            .string()
-            .describe("Name of the passenger, in title case"),
-          flightNumber: z.string().describe("Flight number"),
-          seat: z.string().describe("Seat number"),
-          departure: z.object({
-            cityName: z.string().describe("Name of the departure city"),
-            airportCode: z.string().describe("Code of the departure airport"),
-            airportName: z.string().describe("Name of the departure airport"),
-            timestamp: z.string().describe("ISO 8601 date of departure"),
-            terminal: z.string().describe("Departure terminal"),
-            gate: z.string().describe("Departure gate"),
-          }),
-          arrival: z.object({
-            cityName: z.string().describe("Name of the arrival city"),
-            airportCode: z.string().describe("Code of the arrival airport"),
-            airportName: z.string().describe("Name of the arrival airport"),
-            timestamp: z.string().describe("ISO 8601 date of arrival"),
-            terminal: z.string().describe("Arrival terminal"),
-            gate: z.string().describe("Arrival gate"),
-          }),
-        }),
-        execute: async (boardingPass) => {
-          return boardingPass;
-        },
-      },
+      //     const weatherData = await response.json();
+      //     return weatherData;
+      //   },
+      // },
+      // displayBoardingPass: {
+      //   description: "Display a boarding pass",
+      //   parameters: z.object({
+      //     reservationId: z
+      //       .string()
+      //       .describe("Unique identifier for the reservation"),
+      //     passengerName: z
+      //       .string()
+      //       .describe("Name of the passenger, in title case"),
+      //     flightNumber: z.string().describe("Flight number"),
+      //     seat: z.string().describe("Seat number"),
+      //     departure: z.object({
+      //       cityName: z.string().describe("Name of the departure city"),
+      //       airportCode: z.string().describe("Code of the departure airport"),
+      //       airportName: z.string().describe("Name of the departure airport"),
+      //       timestamp: z.string().describe("ISO 8601 date of departure"),
+      //       terminal: z.string().describe("Departure terminal"),
+      //       gate: z.string().describe("Departure gate"),
+      //     }),
+      //     arrival: z.object({
+      //       cityName: z.string().describe("Name of the arrival city"),
+      //       airportCode: z.string().describe("Code of the arrival airport"),
+      //       airportName: z.string().describe("Name of the arrival airport"),
+      //       timestamp: z.string().describe("ISO 8601 date of arrival"),
+      //       terminal: z.string().describe("Arrival terminal"),
+      //       gate: z.string().describe("Arrival gate"),
+      //     }),
+      //   }),
+      //   execute: async (boardingPass) => {
+      //     return boardingPass;
+      //   },
+      // },
     },
     experimental_telemetry: {
       isEnabled: false,
