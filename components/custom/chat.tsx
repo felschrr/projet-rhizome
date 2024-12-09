d31d3aa91533d2fede53f6676abd4f3c8417d954
Félix SCHERER <felix.scherer@laposte.net>
2024-12-09 15:13:33 +0100

    chore: cleaning code

--- components/custom/chat.tsx
+++ components/custom/chat.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Attachment, Message } from "ai";
+import { Message } from "ai";
 import { useChat } from "ai/react";
 
 import { Message as PreviewMessage } from "@/components/custom/message";
