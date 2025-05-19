789949c75129e1fda65097170f6e219ecbf37ff0
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2025-05-19 13:19:31 +0200

    fix: correct logo image filename casing in Footer components

--- app/(home)/Footer.tsx
+++ app/(home)/Footer.tsx
@@ -7,7 +7,7 @@ export function Footer (){
       <div>
         <div className="flex justify-center mb-4">
           <a href="/" className="flex items-center gap-2">
-            <Image src="/images/Logo_footer.svg" alt="Logo" width={305} height={120} />
+            <Image src="/images/logo_footer.svg" alt="Logo" width={305} height={120} />
           </a>
         </div>
       </div>
